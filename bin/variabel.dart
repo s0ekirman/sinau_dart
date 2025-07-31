88752f2ef01b0ac4a315ff0fed63a88eb383a7d4
Lenovo <mcpemcpc9@gmail.com>
2025-07-31 14:17:20 +0700

    commit

---
+++ bin/variabel.dart
@@ -0,0 +1,19 @@
+void main(){
+//   ra efisien/ tidak efisien
+
+  //   print("rofiq budiyanto");
+  //   print("rofiq budiyanto");
+  //   print("rofiq budiyanto");
+  //   print("rofiq budiyanto");
+  //   print("rofiq budiyanto");
+
+  String name;
+
+  name = 'rofiq budiyanto';
+
+  print("hai,nama saya "+ name);
+  print("hai,nama saya $name");
+  print("hai,nama saya "+ name);
+  print("hai,nama saya $name");
+
+}
